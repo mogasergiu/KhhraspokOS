07ca1c05891c69faa71e8a9ad47c7f5ad4984f95
Sergiu Moga <sergiu.moga@protonmail.com>
2021-07-31 21:05:53 +0300

    [src/kernel]: Added `Text Mode` `VGA`
    This commit adds some basic printing functionality through `VGA`'s `Text Mode`.

---
+++ src/kernel/includes/video.hpp
@@ -0,0 +1,90 @@
+#include <stdint.h>
+#include <stddef.h>
+
+#ifndef VIDEO_HPP
+#define VIDEO_HPP
+
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 20
+#define VGA_START_ADDRESS 0xb8000
+
+// This namespace contains every Video related component
+namespace VIDEO {
+
+    // This namespace contains every VGA related component
+    namespace VGA {
+
+        // This class contains the Text Mode functionality that is part of VGA
+        class TextMode {
+            private:
+                // Pointer to video memory address (0xb8000)
+                uint16_t *address;
+
+                // Represents the line we are currently at inside VGA buffer
+                uint8_t line
+;
+                // Represents the column we are currently at inside VGA buffer
+                uint8_t column;
+
+            public:
+                // Constructor - sets up line, column, address and VGA buffer
+                TextMode();
+
+                /*
+                 * Places given character of given color in VGA buffer
+                 * @character: the character the be placed in the buffer
+                 * @color: the desired color of the character
+                 */
+                void putChar(const char character, const uint8_t color);
+
+                /*
+                 * Places given string of given color in VGA buffer
+                 * @string: string to be placed in VGA buffer
+                 * @color: desired color of the string
+                 * @return: number of bytes successfully placed in VGA buffer
+                 */
+                size_t putString(const char* string, const uint8_t color);
+
+                /*
+                 * Places first number of given bytes of given string of given
+                 * color in VGA buffer
+                 * @string: string to be placed in VGA buffer
+                 * @color: desired color of the string
+                 * @length: number of first bytes of given string to be placed
+                 * @return: number of bytes successfully placed in VGA buffer
+                 */
+                size_t putString(const char* string, const uint8_t color,
+                                    const size_t length);
+
+                /*
+                 * Retrieves current line of our position in the VGA buffer
+                 * @return: current line of our position in the VGA buffer
+                 */
+                uint8_t getLine() const;
+
+                /*
+                 * Retrieves current column of our position in the VGA buffer
+                 * @return: current column of our position in the VGA buffer
+                 */
+                uint8_t getColumn() const;
+
+                /*
+                 * Sets current line in the VGA buffer
+                 * @line: given line to set
+                 * @return: the new line or the current line if given line is
+                 *           out of bounds
+                 */
+                uint8_t setLine(const uint8_t line);
+
+                /*
+                 * Sets current column in the VGA buffer
+                 * @column: given column to set
+                 * @return: the new column or the current column if given
+                 *          column is out of bounds
+                 */
+                uint8_t setColumn(const uint8_t column);
+        };
+    };
+}
+
+#endif /* VIDEO_HPP */
---
+++ src/kernel/video/VGA/textMode.cpp
@@ -0,0 +1,126 @@
+#include <video.hpp>
+#include <string.hpp>
+
+using namespace VIDEO;
+
+// Constructor - sets up line, column, address and VGA buffer
+VGA::TextMode::TextMode() {
+    this->column = this->line = 0;
+
+    this->address = (uint16_t*)VGA_START_ADDRESS;
+
+    // Clear screen (fill with black zeroes)
+    for (uint8_t l = 0; l < VGA_HEIGHT; l++) {
+        for (uint8_t c = 0; c < VGA_WIDTH; c++) {
+            this->address[l * VGA_WIDTH + c] = 0x0032;
+        }
+    }
+}
+
+/*
+ * Places given character of given color in VGA buffer
+ * @character: the character the be placed in the buffer
+ * @color: the desired color of the character
+ */
+void VGA::TextMode::putChar(const char character, const uint8_t color) {
+    // If character is newline, move to next line and reset column
+    if (character == '\n') {
+        this->line++;
+        this->column = 0;
+
+    } else {
+        // Pace character of given color in VGA buffer at our current position
+        this->address[VGA_WIDTH * this->line + this->column] =
+                                                    (color << 8) + character;
+
+        // Move cursor forward
+        this->column++;
+
+        // If end of the line reached, we move to next line and reset column
+        if (this->column >= VGA_WIDTH) {
+            this->column = 0;
+            this->line++;
+        }
+    }
+};
+
+/*
+ * Places given string of given color in VGA buffer
+ * @string: string to be placed in VGA buffer
+ * @color: desired color of the string
+ * @return: number of bytes successfully placed in VGA buffer
+ */
+size_t VGA::TextMode::putString(const char* string, const uint8_t color) {
+    size_t length = strlen(string), l;
+
+    for (l = 0; l < length; l++) {
+        putChar(string[l], color);
+    }
+
+    return l;
+}
+
+/*
+ * Places first number of given bytes of given string of given
+ * color in VGA buffer
+ * @string: string to be placed in VGA buffer
+ * @color: desired color of the string
+ * @length: number of first bytes of given string to be placed
+ * @return: number of bytes successfully placed in VGA buffer
+ */
+size_t VGA::TextMode::putString(const char* string, const uint8_t color,
+                                        const size_t length) {
+    size_t l;
+
+    for (l = 0; l < length; l++) {
+        putChar(string[l], color);
+    }
+
+    return l;
+}
+
+/*
+ * Retrieves current line of our position in the VGA buffer
+ * @return: current line of our position in the VGA buffer
+ */
+uint8_t VGA::TextMode::getLine() const {
+    return this->line;
+}
+
+/*
+ * Retrieves current column of our position in the VGA buffer
+ * @return: current column of our position in the VGA buffer
+ */
+uint8_t VGA::TextMode::getColumn() const {
+    return this->column;
+}
+
+/*
+ * Sets current line in the VGA buffer
+ * @line: given line to set
+ * @return: the new line or the current line if given line is
+ *           out of bounds
+ */
+uint8_t VGA::TextMode::setLine(const uint8_t line) {
+    if (line <= VGA_HEIGHT) {
+        this->line = line;
+
+    }
+
+    return this->line;
+}
+
+/*
+ * Sets current column in the VGA buffer
+ * @column: given column to set
+ * @return: the new column or the current column if given
+ *          column is out of bounds
+ */
+uint8_t VGA::TextMode::setColumn(const uint8_t column) {
+    if (column <= VGA_WIDTH) {
+        this->column = column;
+    }
+
+    return this->column;
+}
+
