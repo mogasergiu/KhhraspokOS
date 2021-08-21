69467f63847cc16c39b0925df2e99b3853c21310
Sergiu Moga <sergiu.moga@protonmail.com>
2021-08-21 21:59:15 +0300

    [src/kernel]: Overwrite `PIC` `IRQ`'s, place global initialization in function
    This commit creates a separate function that overwrites the `IRQ`'s handled
    by the `PIC` and places global variables initialization in separate function.

--- src/kernel/kernelMain.cpp
+++ src/kernel/kernelMain.cpp
@@ -4,6 +4,7 @@
 #include <kernel.hpp>
 #include <kpkheap.hpp>
 #include <paging.hpp>
+#include <drivers.hpp>
 
 VIDEO::VGA::TextMode vgaHandler;
 INTERRUPTS::Interrupts intsHandler;
@@ -13,15 +14,36 @@ MMU::PgMgr pageManager;
 
 void* KPKHEAP::topChunk = (void*)KPKHEAP_START;
 
-extern "C" void kernelMain() {
-    KPKHEAP::topChunk = (void*)KPKHEAP_START;
-
+static inline void secondaryPICinit() {
     intsHandler.setIDTEntry(PIC1_IRQ0 + PIC1_IRQ_TIMER,
                             INTERRUPTS::IntHandlers::pitIRQHandler);
     intsHandler.setIDTEntry(PIC1_IRQ0 + PIC1_IRQ_KEYBOARD,
                             INTERRUPTS::IntHandlers::keyboardIRQHandler);
+    for (int i = PIC1_IRQ0 + PIC1_IRQ_SERIAL2;
+        i <= PIC1_IRQ0 + PIC1_IRQ_PARALLEL1; i++) {
+        intsHandler.setIDTEntry(i, INTERRUPTS::IntHandlers::doNothingIRQHandler);
+    }
+    for (int i = PIC2_IRQ8; i <= PIC2_IRQ8 + PIC2_IRQ_HDDC; i++) {
+        if (i == PIC2_IRQ8 + PIC2_IRQ_RESERVED_INT_2 ||
+            i == PIC2_IRQ8 + PIC2_IRQ_RESERVED_INT_3) {
+            continue;
+        }
+
+        intsHandler.setIDTEntry(i, INTERRUPTS::IntHandlers::doNothingIRQHandler);
+    }
+}
+
+static inline void secondaryPreKernel() {
+    KPKHEAP::topChunk = (void*)KPKHEAP_START;
+    DRIVERS::DISK::nail = 0;
+}
+
+extern "C" void kernelMain() {
+    secondaryPreKernel();
+
+    secondaryPICinit();
 
     vgaHandler.putString("hello", 15);
 
-    __asm__ __volatile__("hlt");
+    while (1) {}
 }
