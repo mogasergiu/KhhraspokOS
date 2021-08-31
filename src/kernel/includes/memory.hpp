739e2d2044bad34530817bf803482c0b215a8ef3
Sergiu Moga <sergiu.moga@protonmail.com>
2021-08-31 23:24:30 +0300

    [src/kernel]: Implement `Page Frame Allocator` and `Page Table Manager`

--- src/kernel/includes/memory.hpp
+++ src/kernel/includes/memory.hpp
@@ -1,12 +1,18 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <string.hpp>
+#include <ds.hpp>
 
 #ifndef PAGING_HPP
 #define PAGING_HPP
 
 #define MEM_REGION_COUNT_ADDR 0x500
 #define MEM_MAP_ADDR 0x510
+
+#define KERNEL_USED_MEM 0x400000
+#define KERNEL_START_ADDR 0x100000
+
+#define START_MEM 0x10000000
 
 #define PDE_G 0b100000000
 #define PDE_S 0b010000000
@@ -16,6 +22,7 @@
 #define PDE_U 0b000000100
 #define PDE_R 0b000000010
 #define PDE_P 0b000000001
+#define PDE_NX (1 << 63)
 
 #define PTE_G 0b100000000
 #define PTE_D 0b001000000
@@ -25,37 +32,79 @@
 #define PTE_U 0b000000100
 #define PTE_R 0b000000010
 #define PTE_P 0b000000001
+#define PTE_NX (1 << 63)
 
 #define PML4T_ADDR 0x1000
 #define PDPT0_aDDR 0x2000
 #define PDT0_ADDR 0x3000
 #define PT0_ADDR 0x4000
 
-#define PDE_ENTRIES_ADDR 0x900000
-#define PTE_ENTRIES_ADDR 0xa00000
-
 #define P_TOTAL_ENTRIES 512
 #define PAGE_SIZE 4096
+
+#define PTidx(vaddr) ((((uintptr_t)vaddr) >> 12) & 0x1ff)
+#define PDidx(vaddr) ((((uintptr_t)vaddr) >> 21) & 0x1ff)
+#define PDTidx(vaddr) ((((uintptr_t)vaddr) >> 30) & 0x1ff)
+#define PML4idx(vaddr) ((((uintptr_t)vaddr) >> 39) & 0x1ff)
+
+#define USABLE_MEM 0x1
+#define RESERVED_MEM 0x2
+#define ACPI_RECLAIMABLE_MEM 0x3
+#define ACPI_NVS_MEM 0x4
+#define BAD_MEM 0x5
+
+#define setPgFlag(pg, flags) (pg = (pgTbl*)(((uintptr_t)pg | flags)))
+#define getPgFlag(pg, flag) ((uintptr_t)pg & (1 << flag))
+#define getPgAddr(pg) (pgTbl*)((((uintptr_t)pg >> (uintptr_t)12) << (uintptr_t)12) & (uintptr_t)0xffffffffff000)
+#define setPgAddr(pg, paddr) (pg = (pgTbl*)(((uintptr_t)pg & (uintptr_t)0xfff0000000000fff) | \
+                             ((((uintptr_t)paddr >> (uintptr_t)12) & (uintptr_t)0xffffffffff) << (uintptr_t)12)))
 
 namespace MMU {
 
     extern uint8_t memRegionCount;
+    extern size_t freeMem;
+    extern size_t usedMem;
+    extern size_t reservedMem;
 
-    extern struct memRegionDescriptor {
-        uint64_t baseAdrr;
+    struct memRegionDescriptor {
+        uint64_t baseAddr;
         uint64_t length;
         uint32_t type;
         uint32_t attr;
-    } __attribute__((packed)) *memMap;
+    } __attribute__((packed));
+
+    struct pgTbl {
+        uint64_t *entries[P_TOTAL_ENTRIES];
+    }__attribute__((aligned(PAGE_SIZE)));
+
+    extern memRegionDescriptor *memMap;
+
+    class PgFrAllocator {
+        private:
+            BitMap *PgsMap;
+            size_t bitmapIdx;
+
+        public:
+            void initPgAlloc();
+            void freePg(void* addr);
+            void freePgs(void* addr, size_t count);
+            void allocPg(void* addr);
+            void allocPgs(void* addr, size_t count);
+            void* reqPg();
+    }; 
 
     class PgMgr {
         private:
-            uintptr_t *PML4T;
+            static PgFrAllocator pgAllocator;
+
+            struct PML4struct {
+                pgTbl *entries[4];
+            } __attribute__((packed)) *PML4;
 
         public:
             PgMgr();
 
-            void mapPage(void *vaddr, void *paddr);
+            void mapPg(void *vaddr, void *paddr);
     };
 }
 
--- src/kernel/memory/paging/paging.cpp
+++ src/kernel/memory/paging/paging.cpp
@@ -1,45 +1,188 @@
 #include <memory.hpp>
 #include <kpkheap.hpp>
 #include <video.hpp>
+#include <string.hpp>
 
 using namespace MMU;
 
-PgMgr::PgMgr() {
-/*    this->PML4T = (uintptr_t*)PML4T_ADDR;
+MMU::memRegionDescriptor* MMU::memMap = (memRegionDescriptor*)MEM_MAP_ADDR;
+size_t MMU::usedMem = KERNEL_USED_MEM;
+size_t MMU::reservedMem;
+size_t MMU::freeMem = START_MEM;
+uint8_t MMU::memRegionCount = *(uint8_t*)MEM_REGION_COUNT_ADDR;
+MMU::PgFrAllocator MMU::PgMgr::pgAllocator;
 
-    for (int pml4tIdx = 0; pml4tIdx < 4; pml4tIdx++) {
-        uintptr_t *PDPT = this->PML4T[pml4tIdx];*/
-/*
-    this->PDE = (uintptr_t*)PDE_ENTRIES_ADDR;
+void PgFrAllocator::initPgAlloc() {
+    size_t memSize = MMU::freeMem, pg;
+    MMU::memRegionCount = *(uint8_t*)MEM_REGION_COUNT_ADDR;
 
-    int startRange = 0;
-    uintptr_t *PTE = (uintptr_t*)PTE_ENTRIES_ADDR;
+    this->PgsMap = (BitMap*)KPKHEAP::kpkZalloc(sizeof(*this->PgsMap));
+    this->PgsMap->initBitMap((memSize  / PAGE_SIZE));
 
-    for (int pde = 0; pde < PDE_TOTAL_ENTRIES; pde++) {
-
-        for (int pte = 0; pte < PTE_TOTAL_ENTRIES; pte++) {
-            PTE[pte] = (startRange + (pte * PAGE_SIZE)) | PTE_P | PTE_R | PTE_U;
+    for (MMU::memRegionDescriptor *md = MMU::memMap;
+            md < (MMU::memMap + MMU::memRegionCount); md++) {
+        if (md->type != USABLE_MEM) {
+            for (pg = (md->baseAddr - (md->baseAddr % PAGE_SIZE));
+                pg < ((md->baseAddr + md->length) +
+                    (md->baseAddr + md->length) % PAGE_SIZE); pg += PAGE_SIZE) {
+                this->PgsMap->set(pg / PAGE_SIZE, true);
+            }
         }
-
-        startRange += PTE_TOTAL_ENTRIES * PAGE_SIZE;
-
-        this->PDE[pde] = (uintptr_t)PTE | PDE_P | PDE_R | PDE_U;
-
-        PTE += PTE_TOTAL_ENTRIES;
     }
-*/
+
+    for (pg = 0; pg < 0x8000; pg += PAGE_SIZE) {
+        this->PgsMap->set(pg / PAGE_SIZE, true);
+    }
+
+    this->bitmapIdx = pg / PAGE_SIZE;
+
+    for (pg = 0x80000; pg < 0xfffff; pg += PAGE_SIZE) {
+        this->PgsMap->set(pg / PAGE_SIZE, true);
+    }
+
+    for (pg = KERNEL_START_ADDR; pg < KERNEL_USED_MEM; pg += PAGE_SIZE) {
+        this->PgsMap->set(pg / PAGE_SIZE, true);
+    }
 }
 
-void PgMgr::mapPage(void *vaddr, void *paddr) {
-/*    if (((uintptr_t)vaddr % PAGE_SIZE) || ((uintptr_t)paddr % PAGE_SIZE)){
+void* PgFrAllocator::reqPg() {
+    size_t len = this->PgsMap->getSize();
+
+    for (; this->bitmapIdx < len * 8; this->bitmapIdx++) {
+        if (this->PgsMap->get(this->bitmapIdx) == true) {
+            continue;
+        }
+
+        this->allocPg((void*)(this->bitmapIdx * PAGE_SIZE));
+
+        __asm__ __volatile__("mov %0, %%r15"::"r" ((void*)(this->bitmapIdx * PAGE_SIZE)));
+
+        this->bitmapIdx++;
+
+        return (void*)((this->bitmapIdx - 1) * PAGE_SIZE);
+    }
+
+    return NULL; // Page Frame Swap to file
+}
+
+void PgFrAllocator::freePg(void* addr) {
+    size_t idx = (size_t)addr / PAGE_SIZE;
+
+    if (this->PgsMap->get(idx) == false) {
         return;
     }
 
-    uintptr_t pde = this->PDE[(uintptr_t)vaddr / (PTE_TOTAL_ENTRIES * PAGE_SIZE)];
+    if (this->PgsMap->set(idx, false)) {
+        MMU::freeMem += PAGE_SIZE;
+        MMU::usedMem -= PAGE_SIZE;
 
-    uintptr_t* pte = (uintptr_t*)(pde & 0xfffff000);
+        if (this->bitmapIdx > idx) {
+            this->bitmapIdx = idx;
+        }
+    }
+}
 
-    pte[(uintptr_t)vaddr % (PTE_TOTAL_ENTRIES * PAGE_SIZE) / PAGE_SIZE] =
-                                        (uintptr_t)paddr | PTE_P | PTE_R | PTE_U;*/
+void PgFrAllocator::freePgs(void* addr, size_t count) {
+    for (size_t idx = 0; idx < count; idx++) {
+        this->freePg((void*)((size_t)addr + (idx * PAGE_SIZE)));
+    }
+}
+
+void PgFrAllocator::allocPg(void* addr) {
+    size_t idx = (size_t)addr / PAGE_SIZE;
+
+    if (this->PgsMap->get(idx) == true) {
+        return;
+    }
+
+    if (this->PgsMap->set(idx, true)) {
+        MMU::freeMem -= PAGE_SIZE;
+        MMU::usedMem += PAGE_SIZE;
+    }
+}
+
+void PgFrAllocator::allocPgs(void* addr, size_t count) {
+    for (size_t idx = 0; idx < count; idx++) {
+        this->allocPg((void*)((size_t)addr + (idx * PAGE_SIZE)));
+    }
+}
+
+PgMgr::PgMgr() {
+    this->pgAllocator.initPgAlloc();
+
+    size_t memSize = START_MEM;
+
+    this->PML4 = (PML4struct*)PML4T_ADDR;
+ 
+    for (size_t pg = KERNEL_USED_MEM; pg < memSize; pg += PAGE_SIZE) {
+        __asm__ __volatile__("mov %0, %%r12"::"r" (pg));
+        this->mapPg((void*)pg, (void*)pg);
+    }
+}
+
+void PgMgr::mapPg(void *vaddr, void *paddr) {
+    size_t idx = PML4idx(vaddr);
+    pgTbl *pdt = NULL,
+          *pd = NULL,
+          *pt = NULL,
+          *pte = NULL,
+          *tmpPg = NULL;
+
+    if (!getPgFlag(this->PML4->entries[idx], PDE_P)) {
+        tmpPg = (pgTbl*)this->pgAllocator.reqPg();
+
+        memset(tmpPg, 0, PAGE_SIZE);
+
+        setPgAddr(pdt, tmpPg);
+        setPgFlag(pdt, PDE_P | PDE_R);
+
+        this->PML4->entries[idx] = pdt;
+
+    }
+
+    pdt = getPgAddr(this->PML4->entries[idx]);
+
+    idx = PDTidx(vaddr);
+    if (!getPgFlag(pdt->entries[idx], PDE_P)) {
+        tmpPg = (pgTbl*)this->pgAllocator.reqPg();
+
+        memset(tmpPg, 0, PAGE_SIZE);
+
+        setPgAddr(pd, tmpPg);
+        setPgFlag(pd, PDE_P | PDE_R);
+
+        pdt->entries[idx] = (uint64_t*)pd;
+
+    }
+
+    pd = getPgAddr(pdt->entries[idx]);
+
+    idx = PDidx(vaddr);
+    if (!getPgFlag(pd->entries[idx], PDE_P)) {
+        tmpPg = (pgTbl*)this->pgAllocator.reqPg();
+
+        memset(tmpPg, 0, PAGE_SIZE);
+
+        setPgAddr(pt, tmpPg);
+        setPgFlag(pt, PDE_P | PDE_R);
+
+        pd->entries[idx] = (uint64_t*)pt;
+
+    }
+
+    pt = getPgAddr(pd->entries[idx]);
+
+    idx = PTidx(vaddr);
+
+//    tmpPg = (pgTbl*)this->pgAllocator.reqPg();
+
+//    __asm__ __volatile__("mov %0, %%r11"::"r"(tmpPg));
+//    memset(tmpPg, 0, PAGE_SIZE);
+
+    setPgAddr(pte, paddr);
+    setPgFlag(pte, PDE_P | PDE_R);
+
+    pt->entries[idx] = 0;
+    pt->entries[idx] = (uint64_t*)pte;
 }
 
