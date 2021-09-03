e25f807d81af1ecb6017c3e3411801ca07456914
Sergiu Moga <sergiu.moga@protonmail.com>
2021-09-03 18:28:07 +0300

    [src/kernel]: Add `printf`, `perror`, `CATCH_FIRE` and `putc` to `libk`

---
+++ src/kernel/libk/includes/stdio.hpp
@@ -0,0 +1,28 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#ifndef STDIO_HPP
+#define STDIO_HPP
+
+#define CATCH_FIRE(condition, message) \
+    do { \
+        if ((condition)) { \
+            perror("[%x]: %s\n", __LINE__, (message)); \
+            perror(""); \
+            __asm__ __volatile__( \
+                "hlt" \
+                : \
+                : \
+            ); \
+        } \
+    } while (0)
+
+void putc(const char c);
+
+void pwarn(const char *s);
+
+void perror(const char *format, ...);
+
+void printf(const char *format, ...);
+
+#endif  /* STDIO_HPP */
---
+++ src/kernel/libk/stdio.cpp
@@ -0,0 +1,93 @@
+#include <video.hpp>
+#include <stdarg.h>
+
+void putc(const char c) {
+    vgaHandler.putChar(c, 15);
+}
+
+void pwarn(const char *s) {
+    vgaHandler.putString(s, 14);
+}
+
+static void printHex(int64_t num) {
+    int64_t hex = 0, remainder;
+    char hexChar;
+
+    for(int64_t hexByte = num; hexByte > 0; hexByte /= 0x10) {
+        remainder = hexByte % 0x10;
+
+        if(remainder < 10) {
+	    remainder =remainder + 0x30;
+
+        } else {
+	    remainder = remainder + 0x37;
+        }
+
+        hex = hex * 100 + remainder;
+    }
+
+    for(int64_t hexByte = hex; hexByte > 0; hexByte = hexByte / 100) {
+        hexChar = hexByte % 100;
+        vgaHandler.putChar(hexChar, 15);
+    }
+}
+
+void perror(const char *format, ...) {
+    va_list ap;
+    va_start(ap, format);
+
+    for (int i = 0; format[i] != '\0'; i++) {
+        if (format[i] == '%') {
+            i++;
+            switch (format[i]) {
+                case 's':
+                    vgaHandler.putString(va_arg(ap, const char*), 12);
+                    break;
+
+                case 'x':
+                case 'X':
+                    printHex(va_arg(ap, int64_t));
+                    break;
+
+                case '%':
+                    vgaHandler.putChar('%', 12);
+                    break;
+            }
+
+        } else {
+            vgaHandler.putChar(format[i], 12);
+        }
+    }
+
+    va_end(ap);
+}
+
+void printf(const char *format, ...) {
+    va_list ap;
+    va_start(ap, format);
+
+    for (int i = 0; format[i] != '\0'; i++) {
+        if (format[i] == '%') {
+            i++;
+            switch (format[i]) {
+                case 's':
+                    vgaHandler.putString(va_arg(ap, const char*), 15);
+                    break;
+
+                case 'x':
+                case 'X':
+                    printHex(va_arg(ap, int64_t));
+                    break;
+
+                case '%':
+                    vgaHandler.putChar('%', 15);
+                    break;
+            }
+
+        } else {
+            vgaHandler.putChar(format[i], 15);
+        }
+    }
+
+    va_end(ap);
+}
