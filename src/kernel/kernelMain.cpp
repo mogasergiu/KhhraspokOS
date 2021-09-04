7578fd647c0fc04f00387f0545eed499c4541b68
Sergiu Moga <sergiu.moga@protonmail.com>
2021-09-04 14:04:09 +0300

    [src/kernel]: Remove ugly `secondaryPICinit` static function

--- src/kernel/kernelMain.cpp
+++ src/kernel/kernelMain.cpp
@@ -9,34 +9,8 @@
 #include <acpi.hpp>
 #include <stdio.hpp>
 
-static inline void secondaryPICinit() {
-    intsHandler.setIDTEntry(PIC1_IRQ0 + PIC1_IRQ_TIMER,
-                            INTERRUPTS::IntHandlers::pitIRQHandler);
-    intsHandler.setIDTEntry(PIC1_IRQ0 + PIC1_IRQ_KEYBOARD,
-                            INTERRUPTS::IntHandlers::keyboardIRQHandler);
-    for (int i = PIC1_IRQ0 + PIC1_IRQ_SERIAL2;
-        i <= PIC1_IRQ0 + PIC1_IRQ_PARALLEL1; i++) {
-        intsHandler.setIDTEntry(i, INTERRUPTS::IntHandlers::doNothingIRQHandler);
-    }
-    for (int i = PIC2_IRQ8; i <= PIC2_IRQ8 + PIC2_IRQ_HDDC; i++) {
-        if (i == PIC2_IRQ8 + PIC2_IRQ_RESERVED_INT_2 ||
-            i == PIC2_IRQ8 + PIC2_IRQ_RESERVED_INT_3) {
-            continue;
-        }
-
-        intsHandler.setIDTEntry(i, INTERRUPTS::IntHandlers::doNothingIRQHandler);
-    }
-
-    for (int i = PIC2_IRQ8 + PIC2_IRQ_HDDC + 1; i < MAX_IDT_ENTRIES; i++) {
-        intsHandler.setIDTEntry(i, INTERRUPTS::IntHandlers::doNothingIRQHandler);
-    }
-}
-
 extern "C" void kernelMain() {
     intsHandler.initInterrupts();
-    picHandler.initPIC();
-    pitHandler.initPIT();
-    secondaryPICinit();
 
     sti();
 
