5c5d4c097612b028f4a62f7117369330e2929f87
Sergiu Moga <sergiu.moga@protonmail.com>
2021-09-07 10:13:51 +0300

    [src/boot]: Increase number of bytes read by bootloader

--- src/boot/bootloader_stage_2.asm
+++ src/boot/bootloader_stage_2.asm
@@ -29,7 +29,7 @@ startLongMode:
     ; Manually moving 8192 bytes at address 0x100000 where our kernel will be
     mov rsi, 0x8400
     mov rdi, 0x100000
-    mov rcx, 0x6000
+    mov rcx, 0x8000
     rep movsb
 
     ; Update stack pointers
