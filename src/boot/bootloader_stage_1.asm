f550583501ab63fd5ea04a34c604692c0ec4279f
Sergiu Moga <sergiu.moga@protonmail.com>
2021-09-08 20:36:44 +0300

    [*]: Prepare for `FAT16`

--- src/boot/bootloader_stage_1.asm
+++ src/boot/bootloader_stage_1.asm
@@ -6,12 +6,30 @@ ORG 0x7c00
 CODE_SEGMENT_OFFSET equ gdtStart.gdtCodeSegmentDescriptor - gdtStart
 DATA_SEGMENT_OFFSET equ gdtStart.gdtDataSegmentDescriptor - gdtStart
 
-; Add a temporary fake BIOS Parameter Block - will update later for FAT
 fakeBPB:
     jmp short _startRealMode
     nop
 
-times 33 db 0
+OEMName db 'KPK OS  '
+bytesPerSector dw 0x200
+sectorsPerCluster db 0x80
+reservedSectorsCount dw 200
+tableCount db 0x02
+rootEntryCount dw 0x40
+totalSectors16 dw 0x00
+mediaType db 0xF8
+tableSize dw 0x100
+sectorsPerTrack dw 0x20
+headSideCount dw 0x40
+hiddenSectorsCount dd 0x00
+totalSectors32 dd 0x773594
+
+BIOSDriverNum db 0x80
+reserved1 db 0x00
+bootSignature db 0x29
+volumeID dd 0xD105
+volumeIDLabel db 'KhhraspokOS'
+FATTypeLabel db 'FAT16   '
 
 ; Real Mode Bootloader:
 ;       - Loads the other sectors into memory
