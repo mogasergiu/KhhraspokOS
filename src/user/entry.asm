3d4c1847275080ffb5616788f0037069242a8e52
Sergiu Moga <sergiu.moga@protonmail.com>
2021-09-30 15:18:37 +0300

    [src/user]: Add the ability to have User classes

--- src/user/entry.asm
+++ src/user/entry.asm
@@ -4,8 +4,66 @@ global _entry
 extern main
 extern exit
 
+; Export constructor and destructor sections
+extern startCtors
+extern endCtors
+extern startDtors
+extern endDtors
+
 _entry:
+    push r15
+    push r14
+    push r13
+    push r12
+    push r11
+    push r10
+    push r9
+    push r8
+    push rbp
+    push rsi
+    push rdi
+    push rdx
+    push rcx
+    push rbx
+    push rax
+    xor rbx, rbx
+    mov ebx, startCtors
+    jmp .ctorsIterate
+
+.callConstructor:
+    call [rbx]
+    add rbx, 8
+.ctorsIterate:
+    cmp rbx, endCtors
+    jb .callConstructor
+
+    pop rax
+    pop rbx
+    pop rcx
+    pop rdx
+    pop rdi
+    pop rsi
+    pop rbp
+    pop r8
+    pop r9
+    pop r10
+    pop r11
+    pop r12
+    pop r13
+    pop r14
+    pop r15
+
     call main
+
+    mov  rbx, endDtors
+    jmp  .dtorsIterate
+.callDestructor:
+    sub  rbx, 8
+    call [rbx]
+.dtorsIterate:
+    cmp  rbx, startDtors
+    ja   .callDestructor
+
     call exit
     jmp $
 
